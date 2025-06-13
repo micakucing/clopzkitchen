e54887d84677d926ff6f0ae987e4b85862a1bbf3
micakucing <ramahardiannsidik@gmail.com>
2025-06-14 06:44:29 +0700

    .

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
 
   return (
     <nav className={styles.navbar}>
-      <h6 className={styles.logo}>GamesKart</h6>
+      <h6 className={styles.logo}>Clopzkicthen</h6>
       <ul className={styles.links}>
         <li className={styles.navlink}>
           <Link href="/">Homes</Link>
