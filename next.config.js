f4f3e964be5bf4316de3a8cb2d35a45c5a5d776b
micakucing <ramahardiannsidik@gmail.com>
2025-06-14 11:46:41 +0700

    .

--- next.config.js
+++ next.config.js
@@ -8,5 +8,17 @@ module.exports = {
   type: "route",
   compiler: {
     removeConsole: true,
-  }
+  },
+  
+  webpack(config, { nextRuntime }) {
+    // as of Next.js latest versions, the nextRuntime is preferred over `isServer`, because of edge-runtime
+    if (typeof nextRuntime === "undefined") {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+      };
+    }
+    
+    return config;
+  } 
 };
