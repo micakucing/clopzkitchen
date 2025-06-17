d03e006e79898b05e86706360c10b9c5a81095ef
micakucing <ramahardiansidik@gmail.com>
2025-06-17 10:52:35 +0700

    .

--- products.js
+++ products.js
@@ -1,56 +1,65 @@
 // DUMMY PRODUCTS (PRODUCT ID : DATA)
 var products = {
     1: {
+        id: 1,
         name: "Cireng ayam (kecil)",
         img: "ayamkecil.jpg",
         price: 1000,
         category: "cireng"
     },
     2: {
+        id: 2,
         name: "Cireng ayam (besar)",
         img: "ayam.jpg",
         price: 2000,
         category: "cireng besar"
     },
     3: {
+        id: 3,
         name: "Cireng ati (kecil)",
         img: "ati.jpg",
         price: 1000,
         category: "cireng"
     },
     4: {
+        id: 4,
         name: "Cireng ati (besar)",
         img: "atibesar.jpg",
         price: 2000,
         category: "cireng"
     }, 
     5: {
+        id: 5,
         name: "Cireng keju (kecil)",
         img: "keju.jpg",
         price: 1000,
         category: "cireng"
     },
     6: {
+        id: 6,
         name: " Cireng keju (besar)",
         img: "kejubesar.jpeg",
         price: 2000,
         category: "cireng"
     },
     7: {
-        name: " Takoyaki",
+        id: 7,
+        name: " Takoyaki / 4 pcs",
         img: "takoyaki.jpeg",
-        price: 2000,
+        price: 5000,
         category: "takoyaki"
     },
 
     8: {
+        id: 8,
         name: " Martabak",
         img: "JS-CART-2.png",
         price: 2000,
         category: "martabak"
     },
 
-    8: {
+    9: {
+        id: 9,
         name: " dimsum",
         img: "dimsum.jpeg",
         price: 2000,
@@ -58,6 +67,7 @@ var products = {
     },
 
     10: {
+        id: 10,
         name: " Risoles ayam pedas / 3 pcs",
         img: "risolayam.jpg",
         price: 5000,
