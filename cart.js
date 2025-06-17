278a5e759a1bd062e014f0b4ad17d29a39647e22
micakucing <ramahardiansidik@gmail.com>
2025-06-17 14:08:09 +0700

    .

--- cart.js
+++ cart.js
@@ -4,7 +4,6 @@ const rupiah = (number) => {
     currency: "IDR"
   }).format(number);
 };
-
 var hargasemua = [];
 var myArray = [];
 var cart = {
@@ -19,14 +18,12 @@ var cart = {
   // (B) LOCALSTORAGE CART
   // (B1) SAVE CURRENT CART INTO LOCALSTORAGE
   save: () => localStorage.setItem("cart", JSON.stringify(cart.items)),
-
   // (B2) LOAD CART FROM LOCALSTORAGE
   load: () => {
     cart.items = localStorage.getItem("cart");
     if (cart.items == null) { cart.items = {}; }
     else { cart.items = JSON.parse(cart.items); }
   },
-
   // (B3) EMPTY ENTIRE CART
   nuke: () => {
     if (confirm("Apa anda yakin akan menghapus semua keranjang belanja anda")) {
@@ -40,12 +37,9 @@ var cart = {
     // (C1) GET HTML ELEMENTS
     cart.hPdt = document.getElementById("cart-products");
     cart.hItems = document.getElementById("cart-items");
-
     // (C2) DRAW PRODUCTS LIST
     cart.hPdt.innerHTML = "";
     let template = document.getElementById("template-product").content, p, item;
-
-
     for (let id in products) {
       p = products[id];
       item = template.cloneNode(true);
@@ -55,19 +49,15 @@ var cart = {
       item.querySelector(".p-add").onclick = () => cart.add(id);
       cart.hPdt.appendChild(item);
     }
-
     // (C3) LOAD CART FROM PREVIOUS SESSION
     cart.load();
     // (C4) LIST CURRENT CART ITEMS
     cart.list();
   },
-
   filter: (e) => {
-
     // (C1) GET HTML ELEMENTS
     cart.hPdt = document.getElementById("cart-products");
     cart.hItems = document.getElementById("cart-items");
-
     // (C2) DRAW PRODUCTS LIST
     cart.hPdt.innerHTML = "";
     let template = document.getElementById("template-product").content, p, item;
@@ -89,10 +79,8 @@ var cart = {
         cart.hPdt.appendChild(item);
       }
     }
-
     // (C3) LOAD CART FROM PREVIOUS SESSION
     cart.load();
-
     // (C4) LIST CURRENT CART ITEMS
     cart.list();
   },
@@ -105,7 +93,6 @@ var cart = {
     for (let key in cart.items) {
       if (cart.items.hasOwnProperty(key)) { empty = false; break; }
     }
-
     // (D2) CART IS EMPTY
     if (empty) {
       item = document.createElement("div");
@@ -113,27 +100,22 @@ var cart = {
       cart.hItems.appendChild(item);
       myArray = []
     }
-
     // (D3) CART IS NOT EMPTY - LIST ITEMS
     else {
       let template = document.getElementById("template-cart").content, p, item;
-
       for (let id in cart.items) {
         //console.log(products)
         item = template.cloneNode(true);
         item.querySelector(".c-del").onclick = () => cart.remove(id);
         item.querySelector(".c-name").textContent = products[id].name;
-
         item.querySelector(".c-qty").value = cart.items[id];
         item.querySelector(".c-qty").onchange = function () {
-
           cart.change(id, this.value);
           //alasql("UPDATE keranjang SET jumlah = "+this.value+" WHERE id ="+products[id].id);
         };
         cart.hItems.appendChild(item);
         cart.total += cart.items[id] * products[id].price;
         object = { name: products[id].name, quantitas: cart.items[id] };
-
       }
       //console.log(object)
       // (D3-3) TOTAL AMOUNT
@@ -148,30 +130,24 @@ var cart = {
       // (D3-4) EMPTY & CHECKOUT
       item = document.getElementById("template-cart-checkout").content.cloneNode(true);
       cart.hItems.appendChild(item);
-
       //element = document.getElementById("message");
       // element.innerHTML = myArray[0].name + '\nTOTAL:' + hargasemua;
     }
   },
-
   // (E) ADD ITEM INTO CART
   add: id => {
     if (cart.items[id] == undefined) { cart.items[id] = 1; }
     else {
       cart.items[id]++;
-
     }
-
     console.log(cart.items[id])
     cart.save(); cart.list();
   },
-
   // (F) CHANGE QUANTITY
   change: (pid, qty) => {
     // (F1) REMOVE ITEM
     if (qty <= 0) {
       myArray = [];
-
       delete cart.items[pid];
       cart.save(); cart.list();
       //console.log(myArray)\\
@@ -179,24 +155,16 @@ var cart = {
       cart.items[pid] = qty;
       cart.total = 0;
       for (let id in cart.items) {
-
         cart.total += cart.items[id] * products[id].price;
         document.getElementById("c-total").innerHTML = `TOTAL: ${rupiah(cart.total)}`;
         hargasemua = [rupiah(cart.total)]
-        //console.log(rupiah(cart.total))
-        //$('#message').html(`TOTAL: ${hargasemua}`)
-        //myArray.push(object);
-
         object = { name: products[id].name, quantitas: cart.items[id] };
-        //console.log(myArray[objIndex].name)
-        //myArray.push(object);
       }
     }
   },
   // (G) REMOVE ITEM FROM CART
   remove: id => {
     delete cart.items[id];
-
     cart.save();
     cart.list();
   },
@@ -229,23 +197,14 @@ var cart = {
     var semuatotal;
     i = 1;
     for (let id in cart.items) {
-      // const li = document.createElement('li');
-      // li.textContent = item;
-      //ul.appendChild(li);
-      // namepro.push(item.name + ' - ( ' + item.quantitas + ' )');
-
       perharga = cart.items[id] * products[id].price;
       semuatotal += cart.items[id] * products[id].price;
       namepro.push(i + '. ' + products[id].name + ' - ( ' + cart.items[id] + ' * ' + products[id].price + ' = ' + perharga + ')')
       i++;
-
     }
-
     totalfinal = encodeURIComponent('\n\nTOTAL BAYAR: ' + rupiah(cart.total) + '\n terima kasih sudah belanja di clopz kitchen')
     content = namepro;
-
     alert("terima kasih sudah belanja cemilan di clopz kitchen \n \n sebentar lagi anda akan di hubungkan ke admin clopz kitchen");
-
     // const message = document.getElementById('message').value;
     const whatsappLink = `https://api.whatsapp.com/send?phone=6285718787490&text=${encodeURIComponent(content.join('\n'))} ${totalfinal}`;
     namepro = [];
@@ -256,7 +215,6 @@ var cart = {
     cart.hItems.innerHTML = "";
     window.open(whatsappLink, '_blank');
     window.location.reload()
-
   }
 };
 window.addEventListener("DOMContentLoaded", cart.init);
